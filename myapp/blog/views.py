8c0591404eb6da1acca774749011403b75339d44
suriyaravichandran <rsuriya119@gmail.com>
2025-04-28 10:57:42 +0530

    post getting models

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -7,14 +7,20 @@ from django.urls import reverse
 # logging
 import logging
 
+# getting data from post model
+from .models import Post
+
+from django.http import Http404
+
 # Create your views here.
 
-posts=[
-        {"id":1,"title":"Post 1","content":"content of post 1"},
-        {"id":2,"title":"Post 2","content":"content of post 2"},
-        {"id":3,"title":"Post 3","content":"content of post 3"},
-        {"id":4,"title":"Post 4","content":"content of post 4"},
-    ]
+# static demo data
+# posts=[
+#         {"id":1,"title":"Post 1","content":"content of post 1"},
+#         {"id":2,"title":"Post 2","content":"content of post 2"},
+#         {"id":3,"title":"Post 3","content":"content of post 3"},
+#         {"id":4,"title":"Post 4","content":"content of post 4"},
+#     ]
 
 
 
@@ -22,14 +28,21 @@ posts=[
 # index page
 def index(request):
     blog_title="Latest Post"
+    posts=Post.objects.all()
     return render(request,"blog/index.html",{'blog_title':blog_title,"posts":posts})
 
 # details page
 def detail(request,post_id):
-     post=next((item for item in posts if item["id"] == int(post_id)),None)
+    try:
+    # getting data from  model post id
+        post=Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404("Post Does Not Exist !")
+    #  getting static data
+    #  post=next((item for item in posts if item["id"] == int(post_id)),None)
     #  logger=logging.getLogger("testing")
     #  logger.debug(f"Post variable is {post}")
-     return render(request,"blog/detail.html",{"post":post})
+    return render(request,"blog/detail.html",{"post":post})
 
 # redirection
 
--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b8erdiy)k*pw6z($e$1s^mz@zu)7tc%0h@44ria*!sn^tyax*m"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
